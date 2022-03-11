6a6768c830035c04af01b5cb60040561a52c7977
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-03-11 15:50:52 +0100

    Added: laravel authentication

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');

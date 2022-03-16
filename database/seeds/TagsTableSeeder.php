c30d7b21f31b3b51efe83e9a475e490a0450ca7f
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-03-16 10:57:12 +0100

    Added: tags seed

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['crime', 'world', 'news', 'food', 'history', 'romance', 'war', 'science'];
+
+        foreach($tags as $tag){
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag);
+            $newTag->save();
+        }
+    }
+}
